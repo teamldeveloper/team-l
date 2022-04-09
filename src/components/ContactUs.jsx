345caf2d8193ee3306b2d4cc5f772e2f8f93ba0d
MahmoodAliL <mahmood07700998512@gmail.com>
2022-04-09 14:22:39 +0300

    create footer

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,8 +5,9 @@ const ContactUs = () => {
   return (
     <div>
       <Title value="Contact us" />
-      <p>
-        You can contact us anytime at 📬<a>teamlteaml100@gmail.com</a>
+      <p className="pt-16 text-lg md:text-xl">
+        You can contact us anytime at 📬
+        <a className="font-medium" href="mailto:teamlteaml100@gmail.com">teamlteaml100@gmail.com</a>
       </p>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
 import SectionWrapper from "./SectionWrapper";
-
+import { FaFacebookF, FaTelegramPlane, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className=" text-white/50 flex flex-col justify-center space-y-2 items-center md:flex-row md:justify-between ">
+      <div className="flex flex-col items-center md:items-start">
+        <SocialMedia />
+        <p className="pt-2">Copyright © 2022 Team-L All Rights Reserved.</p>
+      </div>
+      <a href="#">Privacy Policy</a>
+    </div>
+  );
 };
 
-export default SectionWrapper(Footer, "footer");
+const SocialMedia = () => {
+  const iconStyle = "h-8 w-8 border-[1px] rounded-full p-1.5 ";
+  return (
+    <div className="flex text-white items-center space-x-2">
+      <FaYoutube className={iconStyle} />
+      <FaFacebookF className={iconStyle} />
+      <FaTelegramPlane className={iconStyle} />
+    </div>
+  );
+};
+
+export default SectionWrapper(Footer, "footer", "bg-black");
